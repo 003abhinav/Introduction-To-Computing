2a50e13703b3243b736e059c452243e365dd49a4
003abhinav <97431801+003abhinav@users.noreply.github.com>
2022-01-10 13:23:28 +0530

    Create output.py

---
+++ Assignment1/output.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================= RESTART: D:/Python/Assignment1.py ======================
+  ========================== Ques1   ==========================
+
+enter a number: 3
+enter another number: 4
+enter another number: 2
+The average of the given number:  3.0
+
+  ========================== Ques2  ==========================
+
+Gross Income: 100000
+Number of dependents: 2
+Taxable Income:  84000 $
+Your tax:  16800.0 $
+
+  ========================== Ques3  ==========================
+
+SID: 21104003
+Name: Abhinav Sharma
+Gender: M
+Course: Electrical
+CGPA: 8
+[21104003, 'Abhinav Sharma', 'M', 'Electrical', 8.0]
+
+  ========================== Ques4  ==========================
+
+List before sorting:  [30, 25, 22, 40, 20]
+List after sorting [20, 22, 25, 30, 40]
+
+  ========================== Ques5  ==========================
+
+Default list:  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+Deleting 'Black' from the list:   ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+Replacing 'Black' & 'Pink' with 'Purple' int the list:
+ ['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
