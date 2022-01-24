251752d05fa30dadbbd443dd79efee92268b7c4f
003abhinav <97431801+003abhinav@users.noreply.github.com>
2022-01-24 16:20:26 +0530

    Create output.py

---
+++ Assignment2/output.py
@@ -0,0 +1,67 @@
+===================== RESTART: D:\Python\A2\Assignment2.py =====================
+  ========================== Ques1   ==========================
+
+String:  Python is a case sensitive language
+
+Length of the string:  36
+
+Reversing the string:  
+egaugnal evitisnes esac a si nohtyP
+
+Slicing:  a case sensitive
+
+Replacing string:  Python is object oriented language
+
+
+Index of 'a':  10
+
+Removing white spaces:  Pythonisacasesensitivelanguage
+
+
+  ========================== Ques2  ==========================
+
+Student Data:  ['Abhinav Sharma', '21104003', 'Electrical', '9.9'] 
+
+Hey,  Abhinav Sharma  Here!
+My SID is  21104003 
+I am from  Electrical  department and my CGPA is  9.9
+
+  ========================== Ques3  ==========================
+
+a: 56 , b: 10 
+
+a&b:  8
+a|b:  58
+a^b:  50
+
+Left shifting 'a' with 2 bits:  224 
+Left shifting 'b' with 2 bits:  40
+
+Right shifting 'a' with 2 bits:  14 
+Right shifting 'b' with 4 bits:  0
+
+  ========================== Ques4  ==========================
+
+Enter any number 
+5923
+Enter any number 
+833
+Enter any number 
+9836
+Greatest number entered by the user  9836
+
+  ========================== Ques5  ==========================
+
+enter any string
+any string
+No, it does not contains 'name'
+
+  ========================== Ques6  ==========================
+
+enter the value of side of triangle
+3
+enter the value of side of triangle
+4
+enter the value of side of triangle
+5
+Yes, triangle can be formed by these sides
