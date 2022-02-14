6b488751140047dc9e4e1db0a73c858d5b09a023
003abhinav <97431801+003abhinav@users.noreply.github.com>
2022-02-14 18:00:49 +0530

    Create output_3.py

---
+++ Assignment3/output_3.py
@@ -0,0 +1,115 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================== RESTART: D:\Python\temp.py ==========================
+
+   ========================== Ques1   ==========================
+
+Enter any string: welcome to my python program
+welcome : 1
+to : 1
+my : 1
+python : 1
+program : 1
+
+   ========================== Ques2   ==========================
+
+Day: 28
+Month: 2
+Year: 2020
+Next Date is:  29 / 2 / 2020
+
+   ========================== Ques3   ==========================
+
+
+Do you want to add number to the list? (Y/N): y
+
+Enter the element of the string: 3
+
+Do you want to add number to the list? (Y/N): y
+
+Enter the element of the string: 9
+
+Do you want to add number to the list? (Y/N): y
+
+Enter the element of the string: 10
+
+Do you want to add number to the list? (Y/N): n
+
+list of tuples with square:  [(3, 9), (9, 81), (10, 100)]
+
+   ========================== Ques4   ==========================
+
+enter your grade: 9
+
+Your Grade is ' A ' and  Excellent Performance
+
+   ========================== Ques5   ==========================
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+   ========================== Ques6   ==========================
+
+
+Do you want to add a student detail?(Y/N): y
+
+Student's name: amit
+Enter your SID: 21104
+
+Do you want to add a student detail?(Y/N): y
+
+Student's name: arjun
+Enter your SID: 21100
+
+Do you want to add a student detail?(Y/N): y
+
+Student's name: rohit
+Enter your SID: 21103
+
+Do you want to add a student detail?(Y/N): n
+
+
+Student details:  {'21104': 'amit', '21100': 'arjun', '21103': 'rohit'} 
+
+Sorting by names:  {'21104': 'amit', '21100': 'arjun', '21103': 'rohit'} 
+
+Sorting by SID:  {'21100': 'arjun', '21103': 'rohit', '21104': 'amit'} 
+
+Enter the SID of the student: 21104
+
+Name of the student: ' amit '
+
+   ========================== Ques7   ==========================
+
+Enter the total terms of the fibonacci series: 10
+
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+
+Average of series:  8.8
+
+   ========================== Ques8   ==========================
+
+part a:  {1, 3, 5, 6, 8} 
+
+part b:  {17, 3, 6, 8, 9, 13} 
+
+part c:  {1, 2, 4, 5} 
+
+part d:  {6, 7, 8, 9, 10} 
+
+part e:  {10, 7} 
+
