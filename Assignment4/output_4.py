9db2faeed26f204a7b2c8b79e4e5c8795675bd96
003abhinav <97431801+003abhinav@users.noreply.github.com>
2022-03-02 22:18:45 +0530

    Create output_4.py

---
+++ Assignment4/output_4.py
@@ -0,0 +1,99 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: D:/Python/A4/assignment4.py =====================
+
+   ========================== Ques1   ==========================
+
+Moving disk 1 from rod A to rod B
+Moving disk 2 from rod A to rod C
+Moving disk 1 from rod B to rod C
+Moving disk 3 from rod A to rod B
+Moving disk 1 from rod C to rod A
+Moving disk 2 from rod C to rod B
+Moving disk 1 from rod A to rod B
+Moving disk 4 from rod A to rod C
+Moving disk 1 from rod B to rod C
+Moving disk 2 from rod B to rod A
+Moving disk 1 from rod C to rod A
+Moving disk 3 from rod B to rod C
+Moving disk 1 from rod A to rod B
+Moving disk 2 from rod A to rod C
+Moving disk 1 from rod B to rod C
+
+   ========================== Ques2   ==========================
+
+Number of rows: 5
+
+using iterative: 
+
+      1
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+using recursion: 
+
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+
+   ========================== Ques3   ==========================
+
+enter integer 1: 17
+enter integer 2: 2
+
+Are quotient/remainder callable:  False / False
+
+Quotient:  8     Remainder:  1
+
+values are non zero
+
+extended list:  [4, 5, 6, 17, 2] 
+
+Filtered list:  [5, 6, 17] 
+
+converted list into set:  {17, 5, 6} 
+
+Immutable set:  frozenset({17, 5, 6})
+
+Max value from the set:  17
+
+   ========================== Ques4   ==========================
+
+Name:  Abhinav Sharma 
+Roll No.:  21104003
+
+object destroyed
+
+   ========================== Ques5   ==========================
+
+Name:  Mehak 
+Salary:  40000 
+
+Name:  Ashok 
+Salary:  50000 
+
+Name:  Viren 
+Salary:  60000 
+
+Updated salary of  Mehak :  70000 
+
+details deleted of  Viren
+
+   ========================== Ques6   ==========================
+
+Friendship game started
+
+Enter name of player 1: George
+
+Enter name of player 2: Barbie
+
+Player 1 enter your word: nap
+
+Player 2 enter your word: pan
+
+Friendship of  George  and  Barbie  is TRUE
